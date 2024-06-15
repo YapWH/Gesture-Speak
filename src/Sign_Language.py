e67287550bf90463b16631ece37537a6e93f6311
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-06-15 11:40:22 +0800

    Add KD

--- src/Sign_Language.py
+++ src/Sign_Language.py
@@ -1,15 +1,16 @@
 import torch
 import random
 import pickle
-import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader, random_split
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
-from torchvision.models import efficientnet_v2_s, EfficientNet_V2_S_Weights
+from torchvision.models import efficientnet_v2_l, EfficientNet_V2_L_Weights, efficientnet_v2_s, EfficientNet_V2_S_Weights
 from torch import nn, optim
 from collections import defaultdict, Counter
+import torch.nn.functional as F
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -34,15 +35,12 @@ class SignLanguageDataset(Dataset):
 class EfficientNet(nn.Module):
     def __init__(self, num_classes):
         super(EfficientNet, self).__init__()
-        weights = EfficientNet_V2_S_Weights.IMAGENET1K_V1
-        self.base_model = efficientnet_v2_s(weights=weights)
+        weights = EfficientNet_V2_L_Weights.IMAGENET1K_V1
+        self.base_model = efficientnet_v2_l(weights=weights)
         num_features = self.base_model.classifier[1].in_features
         self.base_model.classifier[1] = nn.Linear(num_features, num_classes)
-        # self.dropout = nn.Dropout(p=0.5)
         
     def forward(self, x):
-        # x = self.base_model(x)
-        # return self.dropout(x)
         return self.base_model(x)
 
 def train(model, criterion, optimizer, train_loader, val_loader, num_epochs, patience):
@@ -51,53 +49,56 @@ def train(model, criterion, optimizer, train_loader, val_loader, num_epochs, pat
     best_val_loss = float('inf')
     patience_counter = 0
 
-    for epoch in range(num_epochs):
-        model.train()
-        running_loss = 0.0
-        for inputs, labels in train_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-
-        train_loss = running_loss / len(train_loader)
-        train_losses.append(train_loss)
-        print(f'Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}')
-
-        model.eval()
-        val_loss = 0.0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for inputs, labels in val_loader:
+    with tqdm(range(num_epochs), desc="Training") as pbar:
+        for epoch in pbar:
+            model.train()
+            running_loss = 0.0
+            
+            for inputs, labels in train_loader:
                 inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
                 outputs = model(inputs)
                 loss = criterion(outputs, labels)
-                val_loss += loss.item()
-                _, predicted = torch.max(outputs, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
 
-        val_loss /= len(val_loader)
-        val_accuracy = correct / total
-        val_losses.append(val_loss)
-        print(f'Validation Accuracy: {val_accuracy:.4f} - Loss: {val_loss:.4f}')
+            train_loss = running_loss / len(train_loader)
+            train_losses.append(train_loss)
 
-        if val_loss < best_val_loss:
-            best_val_loss = val_loss
-            patience_counter = 0
-            torch.save(model.state_dict(), './model/best_model.pth')
-        else:
-            patience_counter += 1
+            model.eval()
+            val_loss = 0.0
+            correct = 0
+            total = 0
 
-        if patience_counter >= patience:
-            print("Early stopping......")
-            break
+            with torch.no_grad():
+                for inputs, labels in val_loader:
+                    inputs, labels = inputs.to(device), labels.to(device)
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+                    val_loss += loss.item()
+                    _, predicted = torch.max(outputs, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+            val_loss /= len(val_loader)
+            val_accuracy = correct / total
+            val_losses.append(val_loss)
+
+            pbar.set_postfix({'Train Loss': train_loss, 'Val Loss': val_loss, 'Val Accuracy': val_accuracy})
+
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                patience_counter = 0
+                torch.save(model.state_dict(), './model/best_model.pth')
+            else:
+                patience_counter += 1
+
+            if patience_counter >= patience:
+                print("Early stopping......")
+                break
 
     return train_losses, val_losses
+
 
 @torch.no_grad()
 def test(model, criterion, test_loader):
@@ -119,6 +120,8 @@ def test(model, criterion, test_loader):
     print(f'Test Accuracy: {accuracy:.4f} - Loss: {test_loss:.4f}')
     
     return test_loss, accuracy
+
+#########################################################################
 
 class NGramModel:
     def __init__(self, n):
@@ -181,12 +184,57 @@ def predict_sequence(model, ngram_model, dataloader, dataset_classes, nn_weight=
     return ''.join(predicted_sequence)
 
 #########################################################################
+
+def knowledge_distillation_loss(teacher_logits, student_logits, true_labels, alpha=0.5, beta=0.5):
+    soft_target_loss = F.kl_div(F.log_softmax(student_logits, dim=1), F.softmax(teacher_logits, dim=1), reduction='batchmean')
+    mse_loss = F.mse_loss(student_logits, F.one_hot(true_labels, num_classes=student_logits.size(1)).float(), reduction='mean')
+    distillation_loss = alpha * soft_target_loss + beta * mse_loss
+    
+    return distillation_loss
+
+def train_student(student_model, teacher_model, device, train_loader, optimizer, alpha, beta, epochs):
+    
+    student_model.train()
+    teacher_model.eval()
+
+    train_loss = []
+
+    with tqdm(range(epochs), desc="Training") as pbar:
+        for epoch in pbar:
+            running_loss = 0.0
+            for inputs, labels in train_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                optimizer.zero_grad()
+                teacher_outputs = teacher_model(inputs)
+                student_outputs = student_model(inputs)
+                loss = knowledge_distillation_loss(teacher_outputs, student_outputs, labels, alpha, beta)
+                loss.backward()
+                optimizer.step()
+                running_loss += loss.item()
+            train_loss.append(running_loss / len(train_loader))
+            pbar.set_postfix({'Train Loss': train_loss[-1]})
+    
+    return train_loss
+
+class EfficientNetSmall(nn.Module):
+    def __init__(self, num_classes):
+        super(EfficientNetSmall, self).__init__()
+        weights = EfficientNet_V2_S_Weights.IMAGENET1K_V1
+        self.base_model = efficientnet_v2_s(weights=weights)
+        num_features = self.base_model.classifier[1].in_features
+        self.base_model.classifier[1] = nn.Linear(num_features, num_classes)
+        
+    def forward(self, x):
+        return self.base_model(x)
+
+#########################################################################
+
 if __name__ == "__main__":
     # Neural Network
     learning_rate = 0.001
-    batch_size = 32
+    batch_size = 16
     num_epochs = 50
-    patience = 5  # Early stopping patience
+    patience = 50  # Early stopping patience
 
     # Define transforms
     transform = transforms.Compose([
@@ -223,9 +271,6 @@ if __name__ == "__main__":
 
     torch.save(model.state_dict(), './model/model.pth')
 
-    # Plot the loss curves and save the plots
-    plt.figure(figsize=(12, 4))
-
     plt.figure(figsize=(10, 5))
     plt.plot(range(1, len(train_losses) + 1), train_losses, label='Train Loss')
     plt.plot(range(1, len(val_losses) + 1), val_losses, label='Validation Loss')
@@ -237,6 +282,9 @@ if __name__ == "__main__":
     plt.show()  # Save the loss curves plot
 
     # N-gram
+    teacher_model = EfficientNet(num_classes=num_classes).to(device)
+    teacher_model.load_state_dict(torch.load('./model/model.pth'))
+    
     num_classes = len(dataset.classes)
     external_sequences = load_external_sequences("./data/data.txt")
     ngram_model = NGramModel(n=2)
@@ -244,7 +292,7 @@ if __name__ == "__main__":
 
     sequences = []
     for inputs, labels in train_loader:
-        _, predicted = torch.max(model(inputs.to(device)), 1)
+        _, predicted = torch.max(teacher_model(inputs.to(device)), 1)
         sequences.append([dataset.classes[label] for label in predicted])
     ngram_model.train(sequences)
 
@@ -254,5 +302,14 @@ if __name__ == "__main__":
 
     pickle.dump(dataset.classes, open("classes.pkl", "wb"))
 
-    predicted_sequences = predict_sequence(model, ngram_model, test_loader, dataset.classes, ngram_weight=0.3, nn_weight=0.7)
-    print(predicted_sequences)
+
+    # predicted_sequences = predict_sequence(model, ngram_model, test_loader, dataset.classes, ngram_weight=0.3, nn_weight=0.7)
+    # print(predicted_sequences)
+
+    student_model = EfficientNetSmall(num_classes=num_classes).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.AdamW(student_model.parameters(), lr=learning_rate)
+
+    train_student(student_model, teacher_model, device, train_loader, optimizer, alpha=0.5, beta=0.5, epochs=50)
+    torch.save(model.state_dict(), './model/student_model.pth')
+    test_loss, test_accuracy = test(model, criterion, test_loader)
