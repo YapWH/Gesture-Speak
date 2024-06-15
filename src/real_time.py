8eb23af28ff6a3ff4ed097d429abf0da7120b14b
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-06-15 22:48:47 +0800

    Update real_time.py
    - Add list for output storage
    - Change predict time to 2 second

--- src/real_time.py
+++ src/real_time.py
@@ -3,6 +3,7 @@ import torch
 import pickle
 from time import time
 import torchvision.transforms as transforms
+from PIL import Image
 
 from Sign_Language import EfficientNetSmall, NGramModel
 
@@ -12,8 +13,9 @@ device = torch.device("cpu") # For testing purposes
 ################################################################################
 
 @torch.no_grad()
-def predict(model, ngram, frame, dataset_classes, nn_weights:float=0.5, ngram_weights:float=0.5):
+def predict(model, ngram, frame, dataset_classes, sequence:list, nn_weights:float=0.5, ngram_weights:float=0.5):
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
     
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -24,39 +26,48 @@ def predict(model, ngram, frame, dataset_classes, nn_weights:float=0.5, ngram_we
     frame = transform(frame).unsqueeze(0).to(device)
     
     model.eval()
-    predicted_sequence = ['<s>']
+    predicted_sequence = sequence
     
     outputs = model(frame)
     nn_probs = torch.softmax(outputs, dim=1)
     _, nn_predicted = torch.max(outputs, 1)
     predicted_labels = [dataset_classes[label] for label in nn_predicted]
+    predicted_sequence.append(predicted_labels[0])
 
     for i, label in enumerate(predicted_labels):
         ngram_predictions = ngram.predict(label)
-        ngram_index = dataset_classes.index(ngram_predictions)
 
-        combined_probs = nn_weights * nn_probs[i] + ngram_weights * (torch.eye(len(dataset_classes))[ngram_index].to(device))
-        corrected_label_index = torch.argmax(combined_probs).item()
-        corrected_label = dataset_classes[corrected_label_index]
-        predicted_sequence[-1] = corrected_label
+        if ngram_predictions == ' ':
+            combined_probs = nn_weights * nn_probs[i]
+            largest_prob = torch.max(combined_probs)
+            if largest_prob >= 0.5:
+                predicted_sequence[-1] = " "
+        else:
+            ngram_index = dataset_classes.index(ngram_predictions)
+
+            combined_probs = nn_weights * nn_probs[i] + ngram_weights * (torch.eye(len(dataset_classes))[ngram_index].to(device))
+            corrected_label_index = torch.argmax(combined_probs).item()
+            corrected_label = dataset_classes[corrected_label_index]
+            predicted_sequence[-1] = corrected_label
     
     return ''.join(predicted_sequence)
 
 ################################################################################
 
 def real_time():
-    model = EfficientNetSmall().to(device)
-    model.load_state_dict(torch.load("../src/model/student_model.pth"))
-    n_gram = NGramModel().load("../src/model/ngram_model.pkl")
-    dataset_classes = pickle.load(open("../src/model/dataset_classes.pkl", "rb"))
+    model = EfficientNetSmall(36).to(device)
+    model.load_state_dict(torch.load("../model/student_model_s1.pth"))
+    n_gram = NGramModel(2).load("../model/ngram_model.pkl")
+    dataset_classes = pickle.load(open("../model/classes.pkl", "rb"))
 
+    cap  = cv2.VideoCapture(0)
     cv2.namedWindow("Sign-Language Recognition", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Sign-Language Recognition", 400, 400)
-    cap  = cv2.VideoCapture(0)
     if not cap:
         raise Exception("Error opening video capture")
     
     start = time()
+    sequence = []
     
     while True:
         ret, frame = cap.read()
@@ -66,8 +77,8 @@ def real_time():
         
         cv2.imshow("Frame", frame)
     
-        if (time() - start) >= 1:
-            print(predict(model, n_gram, frame, dataset_classes, nn_weights=0.7, ngram_weights=0.3))
+        if (time() - start) >= 2:
+            print(predict(model, n_gram, frame, dataset_classes, sequence, nn_weights=0.7, ngram_weights=0.3))
             start = time()
 
         # Exit 
